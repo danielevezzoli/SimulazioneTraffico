ea0f3cafaf33053938516157deba417dc0c72254
LAPTOP-PEQOF1PS\Matteo <matteozanolla@gmail.com>
2017-05-30 18:22:57 +0200

    ok questo va, il menu non sono convinto che così sia funzionale, ma per ora lascio così. clearstrada() praticamente è  una bomba che uccide tutti e lascia intatta la strada. ciao c:

--- src/it/unibs/ing/fp/simulazionetraffico/Simulazione.java
+++ src/it/unibs/ing/fp/simulazionetraffico/Simulazione.java
@@ -9,52 +9,54 @@ import java.util.concurrent.TimeUnit;
 
 public class Simulazione {
 
-//	public static void main(String[] args) {
-//		Strada strada = new Strada(20,10); //Lunghezza, altezza
-//		
-//		final String[] vociMenu = { "Nuova simulazione" , "Avanza simulazione" , "Azzera simulazione" };
-//		MyMenu menu = new MyMenu("Clinica Medica", vociMenu);
-//		
-//		boolean esci = false;
-//		
-//		while (!esci) {
-//			switch (menu.scegli()) {
-//				case 1:
-//					strada = new Strada(20,10);
-//					clearScreen();
-//					strada.paintStrada();
-//					break;
-//				case 2:
-//					strada.avanzaSimulazione();
-//					strada.paintStrada();
-//					break;
-//				case 3:
-//					clearScreen();
-//					strada.clearStrada();
-//					strada.paintStrada();
-//					break;
-//				case 0:
-//					clearScreen();
-//					strada = null;
-//					esci = true;
-//					break;
-//				default:
-//					break;
-//			}
-
 	public static void main(String[] args) throws InterruptedException {
-		Strada strada = new Strada(50, 10); // Lunghezza, altezza
+		Strada strada = new Strada(20,10); //Lunghezza, altezza
 		int timeSimulation = 400;// Millisecondi
 
-		for (; true;) {
-			strada.paintStrada();
-			strada.avanzaSimulazione();
-			clearScreen();
-			TimeUnit.MILLISECONDS.sleep(timeSimulation);
+		
+		final String[] vociMenu = { "Nuova simulazione" , "Avanza simulazione" , "Azzera simulazione" };
+		MyMenu menu = new MyMenu("Clinica Medica", vociMenu);
+		
+		boolean esci = false;
+		
+		while (!esci) {
+			switch (menu.scegli()) {
+				case 1:
+					strada = new Strada(20,10);
+					clearScreen();
+					strada.paintStrada();
+					break;
+				case 2:
+					strada.avanzaSimulazione();
+					strada.paintStrada();
+					break;
+				case 3:
+					clearScreen();
+					strada.clearStrada();
+					strada.paintStrada();
+					break;
+				case 0:
+					clearScreen();
+					strada = null;
+					esci = true;
+					break;
+				default:
+					break;
+			}
 		}
 	}
 
-
+//	public static void main(String[] args) throws InterruptedException {
+//		Strada strada = new Strada(50, 10); // Lunghezza, altezza
+//		int timeSimulation = 400;// Millisecondi
+//
+//		for (; true;) {
+//			strada.paintStrada();
+//			strada.avanzaSimulazione();
+//			clearScreen();
+//			TimeUnit.MILLISECONDS.sleep(timeSimulation);
+//		}
+//	}
 		public static void clearScreen() {
 			System.out.print("\n\n\n\n\n\n\n\n");
 		}
